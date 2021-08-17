e2c154f887250e8547094cd35070c1814192f668
Ayman S15 <48180533+AymanS15@users.noreply.github.com>
2021-08-17 16:30:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,19 +6,23 @@
         onSliderLoad: function() {
             $('#autoWidth').removeClass('cS-hidden');
         } 
-    });
-      $('#autoWidth2').lightSlider({
+    });  
+  });
+   $(document).ready(function() {
+    $('#autoWidth2').lightSlider({
         autoWidth:true,
         loop:true,
         onSliderLoad: function() {
             $('#autoWidth2').removeClass('cS-hidden');
-        } 
+        }
     });
-      $('#autoWidth3').lightSlider({
+  });
+   $(document).ready(function() {
+    $('#autoWidth3').lightSlider({
         autoWidth:true,
         loop:true,
         onSliderLoad: function() {
             $('#autoWidth3').removeClass('cS-hidden');
-        } 
+        }
     });
   });
